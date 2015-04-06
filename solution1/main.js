06e483158927e025cdf7e378128bc4ca122c3fdf
Wojciech Dynus <wojciech.dynus@gmail.com>
2015-04-06 16:00:52 +0200

    removed unnecessary functionality from solution 1

--- solution1/main.js
+++ solution1/main.js
@@ -27,32 +27,24 @@ app.controller("mainController", function($scope) {
         desc: "Product One - description Nunc imperdiet viverra nulla, vulputate aliquam lacus vehicula nec. Vivamus condimentum ut quam tristique bibendum. Sed mattis consequat porta. Pellentesque at congue elit. Pellentesque vel ligula ac sem feugiat egestas quis id massa. Integer fringilla turpis vitae consequat porttitor. Curabitur accumsan, nisl vel efficitur consectetur, lacus velit molestie magna, eget pulvinar magna eros et massa. Maecenas efficitur tellus ac tortor venenatis, sit amet interdum ligula tempor. Pellentesque eros nisl, vestibulum ut varius ac, ultrices sed nisl. desc 1 - end",
         image: "http://placehold.it/350x250",
         price: 100,
-        isSpecial: false,
-        inBasket: false,
         fullDesc: false
     }, {
         name: "Product 2",
         desc: "Product two - description Nunc imperdiet viverra nulla, vulputate aliquam lacus vehicula nec. Vivamus condimentum ut quam tristique bibendum. Sed mattis consequat porta. Pellentesque at congue elit. Pellentesque vel ligula ac sem feugiat egestas quis id massa. Integer fringilla turpis vitae consequat porttitor. Curabitur accumsan, nisl vel efficitur consectetur, lacus velit molestie magna, eget pulvinar magna eros et massa. Maecenas efficitur tellus ac tortor venenatis, sit amet interdum ligula tempor. Pellentesque eros nisl, vestibulum ut varius ac, ultrices sed nisl. desc 2 - end",
         image: "http://placehold.it/350x250",
         price: 100,
-        isSpecial: false,
-        inBasket: false,
         fullDesc: false
     }, {
         name: "Product 3",
         desc: "Product three - description Nunc imperdiet viverra nulla, vulputate aliquam lacus vehicula nec. Vivamus condimentum ut quam tristique bibendum. Sed mattis consequat porta. Pellentesque at congue elit. Pellentesque vel ligula ac sem feugiat egestas quis id massa. Integer fringilla turpis vitae consequat porttitor. Curabitur accumsan, nisl vel efficitur consectetur, lacus velit molestie magna, eget pulvinar magna eros et massa. Maecenas efficitur tellus ac tortor venenatis, sit amet interdum ligula tempor. Pellentesque eros nisl, vestibulum ut varius ac, ultrices sed nisl. desc 3 - end",
         image: "http://placehold.it/350x250",
         price: 300,
-        isSpecial: false,
-        inBasket: false,
         fullDesc: false
     }, {
         name: "Product 4 - special offer",
         desc: "Special product - desc Nunc imperdiet viverra nulla, vulputate aliquam lacus vehicula nec. Vivamus condimentum ut quam tristique bibendum. Sed mattis consequat porta. Pellentesque at congue elit. Pellentesque vel ligula ac sem feugiat egestas quis id massa. Integer fringilla turpis vitae consequat porttitor. Curabitur accumsan, nisl vel efficitur consectetur, lacus velit molestie magna, eget pulvinar magna eros et massa. Maecenas efficitur tellus ac tortor venenatis, sit amet interdum ligula tempor. Pellentesque eros nisl, vestibulum ut varius ac, ultrices sed nisl. desc 4 - end",
         image: "http://placehold.it/350x250",
         price: 40,
-        isSpecial: true,
-        inBasket: false,
         fullDesc: false
     }];
 });
